774dfc6e72509861aa93f8ea5b3028ec578f2c10
Dan <danporeda@yahoo.com>
2023-12-13 13:19:19 -0500

    parralel and sequential promises

--- app.js
+++ app.js
@@ -14,34 +14,34 @@
 // }
 
 
-const moveX = (element, amount, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const bodyBoundary = document.body.clientWidth;
-            const elRight = element.getBoundingClientRect().right;
-            const currLeft = element.getBoundingClientRect().left;
-            if (elRight + amount > bodyBoundary) {
-                reject({ bodyBoundary, elRight, amount });
-            } else {
-                element.style.transform = `translateX(${currLeft + amount}px)`;
-                resolve();
-            }
-        }, delay);
-    })
-};
+// const moveX = (element, amount, delay) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const bodyBoundary = document.body.clientWidth;
+//             const elRight = element.getBoundingClientRect().right;
+//             const currLeft = element.getBoundingClientRect().left;
+//             if (elRight + amount > bodyBoundary) {
+//                 reject({ bodyBoundary, elRight, amount });
+//             } else {
+//                 element.style.transform = `translateX(${currLeft + amount}px)`;
+//                 resolve();
+//             }
+//         }, delay);
+//     })
+// };
 
-const btn = document.querySelector('button');
-async function animateRight(el, amt){
-  await moveX(el,amt,1000);
-  await moveX(el, amt, 1000);
-  await moveX(el, amt, 1000);
-  await moveX(el, amt, 1000);
-}
+// const btn = document.querySelector('button');
+// async function animateRight(el, amt){
+//   await moveX(el,amt,1000);
+//   await moveX(el, amt, 1000);
+//   await moveX(el, amt, 1000);
+//   await moveX(el, amt, 1000);
+// }
 
-animateRight(btn, 100).catch((err) => {
-  console.log('all done');
-  animateRight(btn, -100);
-})
+// animateRight(btn, 100).catch((err) => {
+//   console.log('all done');
+//   animateRight(btn, -100);
+// })
 
 
 // moveX(btn, 300, 1000)
@@ -52,3 +52,64 @@ animateRight(btn, 100).catch((err) => {
 //     .catch(({ bodyBoundary, elRight, amount }) => {
 //         console.log(`You are short ${(Math.round(elRight + amount) - bodyBoundary)} pixels of space`);
 //     });
+
+
+// async function get3Pokemon() {
+//   const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//   const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//   const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// async function get3Pokemon() {
+//   const poke1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//   const poke2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//   const poke3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//   console.log(poke1);
+//   await poke1;
+//   await poke2;
+//   await poke3;
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// async function get3Pokemon() {
+//   const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
+//   const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
+//   const prom3 = axios.get('https://pokeapi.co/api/v2/pokemon/3');
+//   console.log(prom1);
+//   const poke1 = await prom1;
+//   const poke2 = await prom2;
+//   const poke3 = await prom3;
+//   console.log(prom1);
+//   console.log(poke1.data);
+//   console.log(poke2.data);
+//   console.log(poke3.data);
+// }
+
+// get3Pokemon();
+
+function changeBodyColor(color,delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  })
+}
+
+async function lightShow() {
+  const p1 = await changeBodyColor('teal', 1000);
+  const p2 = await changeBodyColor('pink', 1000);
+  const p3 = await changeBodyColor('indigo', 1000);
+  const p4 = await changeBodyColor('violet', 1000);
+  // await p1;
+  // await p2;
+  // await p3;
+  // await p4;
+}
+
+lightShow();
